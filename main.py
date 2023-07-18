1fef86c20159d6e670d127698147cce467227b01
«Alex18» <alexburtsev18@yandex.ru>
2023-07-18 16:57:02 +0300

    первую задачку решил

--- main.py
+++ main.py
@@ -1,1 +1,13 @@
-print('Hello world')
+n = int(input("Введите количество монет: "))
+count_reshka = 0
+count_orel = 0
+for i in range(n):
+    i = int(input("Определите сторону монеток. отрицательные = решка, положительные(от ноля) = орел. Бросайте "))
+    if i < 0:
+        count_reshka += 1
+    elif i > 0:
+        count_orel += 1
+if count_orel >= count_reshka:
+    print(count_reshka)
+else:
+    print(count_orel)
